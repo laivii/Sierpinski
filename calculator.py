a3732f02c6cd37ec59fd0605addee1e6faf4c7a4
laivii <91199307+laivii@users.noreply.github.com>
2022-11-08 17:02:15 +0200

    more test runs

--- calculator.py
+++ calculator.py
@@ -1,17 +1,18 @@
 import png, time, random
 
-#Calculates the coordinates for the next point (always 2/3ds of the distance between the two points)
+#Calculates the coordinates for the next point (always 2/3rds of the distance between the two points towards the end point)
 def calculateNextOne (point1,point2):
+    #Calculating the coordinates x and y for the next point by using the coordinates of our starting and end point
     x = int(point1[0] + ((point2[0]-point1[0])*0.66))
     y = int(point1[1] + ((point2[1]-point1[1])*0.66))
 
-    new_point = [x,y]
+    new_point = [x,y] #Saving them together as a list
 
-    return new_point
+    return new_point #Returning it so it can be used later
 
-#This function returns the coordinates of the hexagons corner that is selected (later) by random number generator
-def whichGon(number):
-    match number:
+#This function returns the coordinates of the hexagons corners, the corner is selected by random number generator
+def whichCorner(corner):
+    match corner:
         case 1:
             coordinates = [250,0]
             return coordinates
@@ -31,44 +32,54 @@ def whichGon(number):
             coordinates = [0,433]
             return coordinates
 
-#Draws the picture
+#Calculates the coordinates and saves them to a list that the function will return for later use
+def calculateCoordinates():
+    startingPoint = [85, 286] #set a the first point (2/3 of 1 --> 6)
+    coordinates = [] #Formatting the list where we will save our coordinates to
+
+    #Calculates x amount of points so the picture starts to form
+    for iteration in range(1, 20_000):
+        cornerPoint = whichCorner(random.randint(1,6)) #Getting the corner by random number in between 1 - 6.
+        point = calculateNextOne(startingPoint,cornerPoint)
+
+        startingPoint = point #Changing the value of the "startingPoint"
+        coordinates.append(point)  #Saving the coordinates to a list
+
+    return coordinates
+
+#Draws the picture by using the coordinates that we have collected
 def drawPicture(coordinates):
-    width = 1001
+    #Here we assign the size of the picture
+    width = 1001 
     height = 1001
+
+    #Here we take the lsit of coordinates so we can use them
     coords = coordinates
     
+    #Formatting the picture so it can be printed (starting it as empty list, here we will collect the information of the color of the pixel)
     img = []
 
+    #This loop goes through our list of coordinates and assign the color for each individual pixel and saves that information to the "img" list
     for y in range(height):
-        row = ()
+        row = () #Forms a tuple where we assign rows
         print(f"On line: {y}", end="\r")
         for x in range(width):
-            color = (255,255,255)
+            color = (255,255,255) #Defaukt color for the pixels
             for i in coords:
+                #This one checks if the coordinate is in our list
                 if  y == i[1] and x == i[0]:
-                    color = (0,0,0)
+                    color = (0,0,0) #If the coordinate is in the list we change the color of that pixel to black
                     break
-            row += color
-        img.append(row)
+            row += color #this assigns the color
+        img.append(row) #Here we append the row to the "row" tuple
 
+    #Here we form the actual picture
     with open('gradient.png', 'wb') as f:
         w = png.Writer(width, height, greyscale=False)
         w.write(f, img)
 
-def calculateCoordinates():
-    aloitusPiste = [85, 286] #set a the first point (2/3 of 1 --> 6)
-    coordinates = []
-
-    #Calculates x amount of points so the picture starts to form
-    for iteration in range(1, 50_000):
-        reunaPiste = whichGon(random.randint(1,6))
-        piste = calculateNextOne(aloitusPiste,reunaPiste)
-
-        aloitusPiste = piste
-        coordinates.append(piste)  
-
-    return coordinates
-
+#Assigning the calculated coordinates here so they can be used
 coordinates = calculateCoordinates()
 
+#Forming the picture
 drawPicture(coordinates)
