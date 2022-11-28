0eeec549864bc36de6984ea2b50e9ed6762ad790
laivii <91199307+laivii@users.noreply.github.com>
2022-11-28 19:54:28 +0200

    scale

--- Javascript 2.0/main.js
+++ Javascript 2.0/main.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
             let y = Math.round(Array[i].y);
             
             context.beginPath();
-            context.rect(x, y, 1, 1);
+            context.rect(x, y, 0.5, 0.5);
             context.fill();
         }
     }
@@ -28,42 +28,40 @@ $(document).ready(function(){
     function chooseShape(){
         //Getting the shape that user has selected
         var selectedShape = $("#shape option:selected").val();
-        console.log(selectedShape);
         var shapesCorners;
         
         /* We select a list based on what shape has been selected
            The list is in form [[startingpoint],[corner1],[corner2],...] (Starting point is always 2/3 between the corner1 and corner2) */
         switch(selectedShape){
             case "triangle":
-                shapesCorners = [[660,67],[0,67],[1000,67],[500,933.02]];
+                shapesCorners = [[0.5],[250,33.5],[0,33.5],[500,33.5],[250,466.51]];
                 break;
             case "square":
-                shapesCorners = [[660,0],[1,1],[1,999],[999,999],[999,1]];
+                shapesCorners = [[0.60],[660,0],[1,1],[1,999],[999,999],[999,1]];
                 break;
             case "pentagon":
-                shapesCorners = [[88,205],[0,534.7],[133.97,34.7],[866.3,34.7],[1000,534.7],[500,965.3]];
+                shapesCorners = [[0.66],[88,205],[0,534.7],[133.97,34.7],[866.3,34.7],[1000,534.7],[500,965.3]];
                 break;
             case "hexagon":
-                shapesCorners = [[580,67],[250,67],[750,67],[1000,500],[750,933],[250,933],[0,500]];
+                shapesCorners = [[0.66],[580,67],[250,67],[750,67],[1000,500],[750,933],[250,933],[0,500]];
                 break;
         }
 
-        console.log(shapesCorners);
         return shapesCorners;
     }
 
     //This function calculates and makes a list of the coordinates we form the picture with
     function calculateCoords(){
         var shape = chooseShape();
-        var startingPoint = shape[0];
-        console.log(startingPoint);
+        var startingPoint = shape[1];
+        var scale = shape[0][0];
         var coordinates = [];
     
         //Calculates x amount of points so the picture starts to form
         for(var i = 0; i < 1000000; i++){
-            var corner = Math.floor(Math.random() * (shape.length-1)) + 1;
+            var corner = Math.floor(Math.random() * (shape.length-2)) + 2;
             var cornerPoint = shape[corner];
-            var point = calculateNextOne(startingPoint, cornerPoint);
+            var point = calculateNextOne(startingPoint, cornerPoint, scale);
     
             startingPoint = point;
     
@@ -74,15 +72,14 @@ $(document).ready(function(){
             
             coordinates[i] = coordObj;
         }
-    
-        console.log(coordinates);
+
         return coordinates;
     }
 
     //This function calculates the next point
-    function calculateNextOne (point1,point2){
-        var x = Math.round(point1[0] + ((point2[0] - point1[0])*0.66));
-        var y = Math.round(point1[1] + ((point2[1] - point1[1])*0.66));
+    function calculateNextOne (point1,point2,scale){
+        var x = Math.round(point1[0] + ((point2[0] - point1[0])*scale));
+        var y = Math.round(point1[1] + ((point2[1] - point1[1])*scale));
     
         var new_point = [x,y];
         
